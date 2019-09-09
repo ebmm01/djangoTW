52bbd1e91cb864491b139497b6116f5ea1d76f4e
Elias Bernardo <ebmm01@gmail.com>
2019-09-08 23:36:40 -0300

    Adding 'editar tarefa' to project

--- to_do/app/services/tarefa_service.py
+++ to_do/app/services/tarefa_service.py
@@ -8,3 +8,17 @@ def cadastrar_tarefa(tarefa):
         data_expiracao = tarefa.data_expiracao,
         prioridade = tarefa.prioridade
     )
+
+def listar_tarefas():
+    return Tarefas.objects.all()
+
+def listar_tarefa_id(id):
+    return Tarefas.objects.get(id=id)
+
+def editar_tarefa(tarefa_bd, tarefa_nova):
+    tarefa_bd.titulo = tarefa_nova.titulo
+    tarefa_bd.descricao = tarefa_nova.descricao
+    tarefa_bd.data_expiracao = tarefa_nova.data_expiracao
+    tarefa_bd.prioridade = tarefa_nova.prioridade
+
+    tarefa_bd.save(force_update=True)
--- to_do/app/views.py
+++ to_do/app/views.py
@@ -6,7 +6,8 @@ from .services import tarefa_service
 # Create your views here.
 
 def listar_tarefas(request):
-    return render(request, 'tarefas/listar_tarefas.html')
+    tarefas = tarefa_service.listar_tarefas()
+    return render(request, 'tarefas/listar_tarefas.html', {"tarefas": tarefas})
 
 def cadastrar_tarefa(request):
     if request.method == 'POST':
@@ -26,4 +27,22 @@ def cadastrar_tarefa(request):
             return redirect('listar_tarefas')
     else:
         form_tarefa = TarefaForm()
+    return render(request, 'tarefas/form_tarefa.html', {"form_tarefa": form_tarefa})
+
+def editar_tarefa(request, id):
+    tarefa_bd = tarefa_service.listar_tarefa_id(id)
+    form_tarefa = TarefaForm(request.POST or None, instance=tarefa_bd)
+    if form_tarefa.is_valid():
+        titulo = form_tarefa.cleaned_data["titulo"]
+        descricao = form_tarefa.cleaned_data["descricao"]
+        data_expiracao = form_tarefa.cleaned_data["data_expiracao"]
+        prioridade = form_tarefa.cleaned_data["prioridade"]
+        tarefa_nova = Tarefa(
+            titulo=titulo,
+            descricao=descricao,
+            data_expiracao=data_expiracao,
+            prioridade=prioridade
+        )
+        tarefa_service.editar_tarefa(tarefa_bd, tarefa_nova)
+        return redirect('listar_tarefas')
     return render(request, 'tarefas/form_tarefa.html', {"form_tarefa": form_tarefa})
