03573123217a76ae3f27c942ea21c2a271a5ceeb
Elias Bernardo <ebmm01@gmail.com>
2019-09-10 22:40:56 -0300

    Adding user field to Tarefas and making migrations

--- to_do/app/models.py
+++ to_do/app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Tarefas(models.Model):
 
     PRIORIDADES_CHOICES = [
@@ -13,3 +14,4 @@ class Tarefas(models.Model):
     descricao = models.CharField(max_length=100, null=False, blank=False)
     data_expiracao = models.DateField(null=False, blank=False)
     prioridade = models.CharField(max_length=1, choices=PRIORIDADES_CHOICES, null=False, blank=False)
+    usuario = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
